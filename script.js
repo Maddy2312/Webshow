014b9f7656d8782255444e194caee63e86f840ad
Maddy2312 <madhav2580sharma@gmail.com>
2024-05-09 19:58:05 +1000

    feature/middle

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+var elemC = document.querySelector("#elem-container");
+var fixImage = document.querySelector("#fixed-image");
+elemC.addEventListener("mouseenter",function(){
+    fixImage.style.display = "block"
+})
+elemC.addEventListener("mouseleave",function(){
+    fixImage.style.display = "none"
+})
+var elems = document.querySelectorAll(".elem");
+elems.forEach(function(e){
+    e.addEventListener("mouseenter",function(){
+        var image = e.getAttribute("data-image")
+        fixImage.style.backgroundImage = `url(${image})`
+    });
+});
